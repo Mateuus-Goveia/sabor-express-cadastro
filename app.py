8875fe7262d905a9744ff2b461c9a3932d78b5bd
Matheus Goveia <mateuus.goveia@gmail.com>
2025-10-24 16:15:44 -0300

    fix: corrige erro ao escolher uma opção inválida
    criado uma validação de try/except e a função opcao_invalida()

--- app.py
+++ app.py
@@ -16,22 +16,31 @@ def exibir_opçoes():
     print('3. Ativar Restaurantes')
     print('4. Sair\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opcão: '))
+def opçao_invalida():
+    print('Opção Inválida\n')
+    input('Precione enter para voltar ao menu principal')
+    main()
 
-    match opcao_escolhida:
-        case 1:
-            print('Cadastrar Restaurantes')
-        case 2:
-            print('Listar Restaurantes')
-        case 3:
-            print('Ativar Restaurantes')
-        case 4:
-            finalizar_app()
-        case _:
-            print('Opção inválida')
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opcão: '))
+
+        match opcao_escolhida:
+            case 1:
+                print('Cadastrar Restaurantes')
+            case 2:
+                print('Listar Restaurantes')
+            case 3:
+                print('Ativar Restaurantes')
+            case 4:
+                finalizar_app()
+            case _:
+                opçao_invalida()
+    except:
+        opçao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opçoes()
     escolher_opcao()

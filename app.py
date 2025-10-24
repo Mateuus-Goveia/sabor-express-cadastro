1caf5f2bb1fb8b36a4cedab3a58573e0aafe11d1
Matheus Goveia <mateuus.goveia@gmail.com>
2025-10-24 15:33:52 -0300

    feat: adciona escolha de opcoes ao menu

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-print('Sabor Express')
+print('''
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀▄▀█ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █░▀░█ █▀▀ █▀▄ ██▄ ▄█ ▄█\n''')
 
 print('1. Cadastrar Restaurantes')
 print('2. Listar Restaurantes')
 print('3. Ativar Restaurantes')
-print('4. Sair')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opcão: '))
+
+match opcao_escolhida:
+    case 1:
+        print('Cadastrar Restaurantes')
+    case 2:
+        print('Listar Restaurantes')
+    case 3:
+        print('Ativar Restaurantes')
+    case 4:
+        print('Encerrando o programa')
+    case _:
+        print('Opção inválida')

b9b2fd3f0788bd2e7d6e356229620257414be180
Matheus Goveia <mateuus.goveia@gmail.com>
2025-10-24 15:57:07 -0300

    refactor: organiza código em funções

--- app.py
+++ app.py
@@ -4,25 +4,37 @@ def finalizar_app():
     os.system('cls')
     print('Encerrando o programa\n')
 
-print('''
+def exibir_nome_do_programa():
+    print('''
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀▄▀█ █▀█ █▀█ █▀▀ █▀ █▀
-▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █░▀░█ █▀▀ █▀▄ ██▄ ▄█ ▄█''')
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █░▀░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+          ''')
 
-print('1. Cadastrar Restaurantes')
-print('2. Listar Restaurantes')
-print('3. Ativar Restaurantes')
-print('4. Sair\n')
+def exibir_opçoes():
+    print('1. Cadastrar Restaurantes')
+    print('2. Listar Restaurantes')
+    print('3. Ativar Restaurantes')
+    print('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opcão: '))
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opcão: '))
 
-match opcao_escolhida:
-    case 1:
-        print('Cadastrar Restaurantes')
-    case 2:
-        print('Listar Restaurantes')
-    case 3:
-        print('Ativar Restaurantes')
-    case 4:
-        finalizar_app()
-    case _:
-        print('Opção inválida')
+    match opcao_escolhida:
+        case 1:
+            print('Cadastrar Restaurantes')
+        case 2:
+            print('Listar Restaurantes')
+        case 3:
+            print('Ativar Restaurantes')
+        case 4:
+            finalizar_app()
+        case _:
+            print('Opção inválida')
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opçoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
